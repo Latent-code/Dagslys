0c5be5a402480168bd7cf729205f4440b8e879c6
Reiel Lien Bruland <reielbruland@gmail.com>
2024-06-17 18:12:35 +0200

    update test

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react"
-import { graphql, navigate, useStaticQuery } from "gatsby"
-import Loading from "../loading/loading"
+import React, { useEffect, useState } from "react";
+import { graphql, navigate, useStaticQuery } from "gatsby";
+import Loading from "../loading/loading";
 
-import MenuItem from "./menuItem"
+import MenuItem from "./menuItem";
 
 const Menu = ({ setMenu, menu, graphiqlData, FolderData, RentalData }) => {
   // const graphiqlData = useStaticQuery(graphql`
@@ -50,15 +50,14 @@ const Menu = ({ setMenu, menu, graphiqlData, FolderData, RentalData }) => {
   //   }
   // `)
 
-  const [isLoading, setIsLoading] = React.useState(true)
-  const hiddenItems = [251, 45, 46, 47]
+  const [isLoading, setIsLoading] = React.useState(true);
+  const hiddenItems = [251, 45, 46, 47];
   const [selectedIndex, setSelectedIndex] = useState(null);
 
-  let completeMenuArr = []
+  let completeMenuArr = [];
   useEffect(() => {
-    
-    console.log("rerendered!!! !!! !  ! ! ! ! ! ! ! ! ! ! !")
-  },[])
+    console.log("rerendered!!! !!! !  ! ! ! ! ! ! ! ! ! ! !");
+  }, []);
   // let FolderData = graphiqlData.allBrentRentalFolder.nodes
   // let RentalData = graphiqlData.allBrentRentalItem.nodes
 
@@ -66,12 +65,12 @@ const Menu = ({ setMenu, menu, graphiqlData, FolderData, RentalData }) => {
     // Change folder and parent to a number, not string
     RentalData.map((item, index) => {
       if (!item.in_shop) {
-        RentalData.splice(index, 1)
+        RentalData.splice(index, 1);
       }
 
-      item.parentFolderId = parseInt(item.folder.split("/").slice(-1))
-      completeMenuArr.push(item)
-    })
+      item.parentFolderId = parseInt(item.folder.split("/").slice(-1));
+      completeMenuArr.push(item);
+    });
 
     FolderData.map((item, index) => {
       if (
@@ -79,61 +78,62 @@ const Menu = ({ setMenu, menu, graphiqlData, FolderData, RentalData }) => {
         item.itemtype === "vehicle" ||
         item.itemtype === "user"
       ) {
-        FolderData.splice(index, 1)
+        FolderData.splice(index, 1);
       } else if (
         item.displayname.startsWith("import-") ||
         item.displayname.startsWith("Import-")
       ) {
       } else if (hiddenItems.includes(item.rentmanId)) {
-        FolderData.splice(index, 1)
+        FolderData.splice(index, 1);
       } else {
         if (item.menuParentBrent != null) {
           item.parentFolderId = parseInt(
-            item.menuParentBrent?.split("/").slice(-1),
-          )
-          completeMenuArr.push(item)
+            item.menuParentBrent?.split("/").slice(-1)
+          );
+          completeMenuArr.push(item);
         } else {
-          item.parentFolderId = null
-          completeMenuArr.push(item)
+          item.parentFolderId = null;
+          completeMenuArr.push(item);
         }
       }
-    })
+    });
 
-    const sort = arr => {
+    const sort = (arr) => {
       // SORT THE MENU BASED ON ORDER IN RENTMAN
-      return arr.sort((a, b) => a.order - b.order)
-    }
+      return arr.sort((a, b) => a.order - b.order);
+    };
+
 
     const menuSort = (function (data, root) {
-        var t = {}
-        data.forEach(o => {
-          Object.assign((t[o.rentmanId] = t[o.rentmanId] || {}), o)
-          ;((t[o.parentFolderId] ??= {}).children ??= []).push(t[o.rentmanId])
-        })
-        return t[root].children
+        var t = {};
+        data.forEach((o) => {
+          Object.assign((t[o.rentmanId] = t[o.rentmanId] || {}), o);
+          ((t[o.parentFolderId] ??= {}).children ??= []).push(t[o.rentmanId]);
+        });
+        return t[root].children;
       })(completeMenuArr, null),
       shop = (r, { children = [], ...o }) => {
-        children = children.reduce(shop, [])
-        const sub = children.length ? { children } : {}
-        if (o.in_shop || sub.children) r.push({ ...o, ...sub })
-        return r
-      }
-    let finalMenu = menuSort.reduce(shop, [])
+        children = children.reduce(shop, []);
+        const sub = children.length ? { children } : {};
+        if (o.in_shop || sub.children) r.push({ ...o, ...sub });
+        return r;
+      };
+    let finalMenu = menuSort.reduce(shop, []);
 
-    setMenu(sort(finalMenu)) // Layout menu setter
-    setIsLoading(false)
-  }
+    setMenu(sort(finalMenu)); // Layout menu setter
+    setIsLoading(false);
+  };
 
   const handleClick = (e, item) => {
-    navigate(item.urlPath)
-  }
+    navigate(item.urlPath);
+  };
 
   useEffect(() => {
-    fixMenu()
+    fixMenu();
     if (menu) {
-      setIsLoading(false)
+      setIsLoading(false);
     }
-  }, [])
+  }, []);
 
   return (
     <div>
@@ -141,22 +141,21 @@ const Menu = ({ setMenu, menu, graphiqlData, FolderData, RentalData }) => {
         <>
           <div>
             {menu ? (
-              menu.map(item => {
-                
+              menu.map((item) => {
                 // REMOVE KITS FROM MENU
                 if (item.rentmanId === 320) {
                 } else {
                   return (
                     <div key={item.id}>
                       <MenuItem
-                        onClick={e => handleClick(e, item)}
+                        onClick={(e) => handleClick(e, item)}
                         menuData={FolderData}
                         item={item}
                         selectedIndex={selectedIndex}
                         setSelectedIndex={setSelectedIndex}
                       ></MenuItem>
                     </div>
-                  )
+                  );
                 }
               })
             ) : (
@@ -168,7 +167,7 @@ const Menu = ({ setMenu, menu, graphiqlData, FolderData, RentalData }) => {
         <Loading></Loading>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
